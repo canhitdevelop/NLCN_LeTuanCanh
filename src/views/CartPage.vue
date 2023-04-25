d4c8ca8158d8c784ece155233218fd6e41f92de1
Le Tuan Canh <ltcanh.itdevelop@mail.com>
2023-04-25 15:14:19 +0700

    25/4

--- src/views/CartPage.vue
+++ src/views/CartPage.vue
@@ -3,18 +3,7 @@
 <template>
   <div id="page-wrap">
     <h1>Shopping Cart</h1>
-    <div
-      v-for="product in cartItems"
-      :key="product.id"
-      class="product-container"
-    >
-      <img class="product-image" :src="product.imageUrl" />
-      <div class="details-wrap">
-        <h3>{{ product.name }}</h3>
-        <p>${{ product.price }}</p>
-      </div>
-      <button class="remove-button">Remove From Cart</button>
-    </div>
+    <ProductsList :products="cartItems" />
     <h3 id="total-price">Total: ${{ totalPrice }}</h3>
     <button id="checkout-button">Proceed to Checkout</button>
   </div>
@@ -22,8 +11,13 @@
 
 <script>
 import { cartItems } from "../fake-data";
+import ProductsList from "../components/ProductsList.vue";
+
 export default {
   name: "CartPage",
+  components: {
+    ProductsList,
+  },
   data() {
     return {
       cartItems,
